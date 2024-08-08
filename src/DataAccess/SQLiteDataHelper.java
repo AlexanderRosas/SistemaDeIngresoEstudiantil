77af018dc487c59c4c093351746e9512cf6f883f
Therinbsv <150133557+Therinbsv@users.noreply.github.com>
2024-08-08 13:40:32 -0500

    Update SQLiteDataHelper.java

--- src/DataAccess/SQLiteDataHelper.java
+++ src/DataAccess/SQLiteDataHelper.java
@@ -9,6 +9,12 @@ public abstract class SQLiteDataHelper {
     private static Connection conn = null;
     // protected SQLiteDataHelper(){}
     
+    /*
+     * Descripcion: Abre la conexión a la base de datos si está abierta.
+     * @throws Exception Si ocurre un error durante el cierre de la conexión.
+     * autor: Katherine Sanchez
+     */
+    
     protected static synchronized Connection openConnection() throws Exception{
         try {
             if(conn == null)
@@ -19,6 +25,11 @@ public abstract class SQLiteDataHelper {
         return conn;
     }
 
+     /*
+     * Descripcion: Cierra la conexión a la base de datos si está abierta.
+     * @throws Exception Si ocurre un error durante el cierre de la conexión.
+     * autor: Katherine Sanchez
+     */
     protected static void closeConnection() throws Exception{
         try {
             if (conn != null)
